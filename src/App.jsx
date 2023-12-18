c30b25d78d827a1326be997d07ceade86f0b5992
shahadkariyambath <shahadchikku@gmail.com>
2023-12-18 12:29:22 +0000

    created otp state and wrote a handleChange to set the opt

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,33 @@ import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [otp, setOtp] = useState({
+    digitOne: "",
+    digitTwo: "",
+    digitThree: "",
+    digitFour: "",
+    digitFive: "",
+    digitSix: "",
+  });
+
+  const handlChnge = (event) => {
+    const { name, value } = event.target;
+
+    setOtp((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   const inputRenderOtp = () => {
     return Object.keys(otp).map((keys, index) => (
       <input
-        ref={(ele) => (inputRef.current[index] = ele)}
         key={index}
         maxLength="1"
         name={keys}
         type="text"
         className="w-16 h-12 rounded-md mr-3 text-center text-xl"
+        onChange={handlChnge}
       />
     ));
   };
