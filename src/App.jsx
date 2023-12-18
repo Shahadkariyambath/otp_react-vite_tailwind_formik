381acf8088407939aaadc74ef56f7b48372d8544
shahadkariyambath <shahadchikku@gmail.com>
2023-12-18 12:28:17 +0000

    function inputRenderOtp is created to render the multiple otp input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const inputOtp = () => {
-    return (
+  const inputRenderOtp = () => {
+    return Object.keys(otp).map((keys, index) => (
       <input
+        ref={(ele) => (inputRef.current[index] = ele)}
+        key={index}
         maxLength="1"
+        name={keys}
         type="text"
         className="w-16 h-12 rounded-md mr-3 text-center text-xl"
       />
-    );
+    ));
   };
 
   return (
     <form action="">
       <h3 className="text-3xl mb-8">Please fill the OTP</h3>
-      <div>
-        {inputOtp()}
-        {inputOtp()}
-        {inputOtp()}
-        {inputOtp()}
-        {inputOtp()}
-        {inputOtp()}
-      </div>
+      <div>{inputRenderOtp()}</div>
       <button className="mt-4 w-32 border border-solid bg-[#3b3b3b] rounded hover:bg-[#252525] hover:border-[#3b3b3b]">
         Submit
       </button>
