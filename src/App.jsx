0cac6452931325bda5c7d31d32cc39ff89794422
shahadkariyambath <shahadchikku@gmail.com>
2023-12-18 12:05:03 +0000

    added form tag and gave necessary CSS style using tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,31 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const inputOtp = () => {
+    return (
+      <input
+        maxLength="1"
+        type="text"
+        className="w-16 h-12 rounded-md mr-3 text-center text-xl"
+      />
+    );
+  };
 
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <form action="">
+      <h3 className="text-3xl mb-8">Please fill the OTP</h3>
+      <div>
+        {inputOtp()}
+        {inputOtp()}
+        {inputOtp()}
+        {inputOtp()}
+        {inputOtp()}
+        {inputOtp()}
+      </div>
+      <button className="mt-4 w-32 border border-solid bg-[#3b3b3b] rounded hover:bg-[#252525] hover:border-[#3b3b3b]">
+        Submit
+      </button>
+    </form>
   );
 }
 
